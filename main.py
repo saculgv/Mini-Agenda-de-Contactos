1c3d341f69681e9d136d16e50d6dcf48eea07a23
saculgv <lucasgajardo26@gmail.com>
2025-06-17 01:47:46 -0400

    feat/main: Se creó menu de agenda

---
+++ main.py
@@ -0,0 +1,43 @@
+import os, time
+os.system("cls")
+
+while True:
+
+    print("\t\t Mini Agenda de Contactos\n")
+    print("1)  Agregar un contacto: nombre, teléfono, email.")
+    print("2)  Listar contactos: mostrar todos los contactos guardados.")
+    print("3)  Buscar un contacto por nombre.")
+    print("4)  Eliminar un contacto.")
+    print("5)  Salir del programa.\n")
+
+    try:
+
+        opcion = int(input("Ingrese una opción.\n"))
+        
+        if opcion == 1:
+            os.system("cls")
+            print("Agregar un contacto:")
+
+        elif opcion == 2:
+            os.system("cls")
+            print("Listar contactos:")
+
+        elif opcion == 3:
+            os.system("cls")
+            print("Buscar un contacto por nombre.")
+
+        elif opcion == 4:
+            os.system("cls")
+            print("Eliminar un contacto.")
+
+        elif opcion == 5:
+            os.system("cls")
+            print("Salir del programa.")
+            print("Chao pecao'...")
+            break
+
+        else:
+            print("La opción ingresada no existe")
+
+    except:
+        print("El valor ingresado debe ser numerico")
